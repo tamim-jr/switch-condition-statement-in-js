168aca9ec1f0fac3b706b8c1805e962f17ffd777
tamim-jr <internationalinf24@gmail.com>
2023-06-27 20:50:24 +0600

    Switch Condition statement in js

---
+++ app.js
@@ -0,0 +1,36 @@
+var input = prompt("Type your Number in 1 to 10")
+switch (input) {
+    case "1":
+        document.write("You type: One");
+        break;
+    case "2":
+        document.write("You type: Two");
+        break;
+    case "3":
+        document.write("You type: Three");
+        break;
+    case "4":
+        document.write("You type: four");
+        break;
+    case "5":
+        document.write("You type: Five");
+        break;
+    case "5":
+        document.write("You type: Six");
+        break;
+    case "7":
+        document.write("You type: Seven");
+        break;
+    case "8":
+        document.write("You type: Eight");
+        break;
+    case "9":
+        document.write("You type: Nine");
+        break;
+    case "10":
+        document.write("You type: Tene");
+        break;
+
+    default:
+        document.write("Your type wrong number");
+}
